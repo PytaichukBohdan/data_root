d953b312f285763252fd27c81b13e1b250c79f31
Pytaichuk Bohdan <woomniazhko@gmail.com>
2017-12-10 10:10:05 +0200

    Module 6

---
+++ Module_6_github/svm/svm/kernel.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class Kernel(object):
+    """Check kernels here https://en.wikipedia.org/wiki/Support_vector_machine"""
+    @staticmethod
+    def linear():
+        return lambda x, y: np.inner(x, y)
+
+    @staticmethod
+    def gaussian(sigma):
+        return lambda x, y: np.exp(-np.power(x - y, 2.) / (2 * np.power(sigma, 2.)))
+
+
